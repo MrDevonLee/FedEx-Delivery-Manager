956fd6f3ece9ac4aaae75b4229cb017224a9a5f7
Devon <devonewo@gmail.com>
2021-08-18 22:58:53 -0500

    Added main .py file.

---
+++ FedExDeliveryManager.py
@@ -0,0 +1,35 @@
+"""
+Name: Devon Lee
+
+Course: CS1430, Section 00,  Fall 2021
+
+Assignment: Assignment 2
+
+Purpose: Make FedEx more effient
+
+Input:   Number of deliveries and the locations of each one for a given route
+
+Output:  Statistics about each route and a highlight of any particularly
+         efficient routes
+
+"""
+
+"""
+CONSTANTS
+"""
+
+
+"""
+Fill in a short program description here. 
+"""
+def main():
+    # Remove the word pass and replace it with your solution.
+    pass
+
+
+"""
+Do not remove the code below this line!
+"""
+
+if __name__ == '__main__':
+    main()
