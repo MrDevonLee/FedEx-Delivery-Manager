4baaf33f89e05414fd39e545f7592eed2bb6005d
Devon <devonewo@gmail.com>
2021-09-01 10:42:06 -0500

    Created logic for inner and outer loops

--- FedExDeliveryManager.py
+++ FedExDeliveryManager.py
@@ -22,14 +22,71 @@ CONSTANTS
 """
 Fill in a short program description here. 
 """
+
+
 def main():
     
-    num_deliveries = 5
-    for i in range(num_deliveries):
-        
-    
-    # Remove the word pass and replace it with your solution.
     pass
+    
+    total_deliveries = 0
+    total_distance = 0
+    
+    shortest_route_distance = 100000000
+    longest_route_distance = -1
+    
+    route_number = 0 # Temporary
+    
+    while route_number < 5:
+        
+        route_number += 1
+        
+        x_curr = 0
+        y_curr = 0
+        route_distance = 0
+        shortest_trip_distance = 100000000
+        longest_trip_distance = -1
+        
+        num_deliveries = int(input())
+       
+        for i in range(num_deliveries):
+            x_next_del, y_next_del = int(input().split(" "))
+            
+            trip_distance = abs(x_curr - x_next_del) + abs(y_curr - y_next_del)
+            route_distance += trip_distance
+            
+            if trip_distance < shortest_trip_distance:
+                shortest_trip_distance = trip_distance
+            elif trip_distance > longest_trip_distance:
+                longest_trip_distance = trip_distance
+    
+            x_curr += x_next_del
+            y_curr += y_next_del
+            
+        average_trip_distance = float(route_distance) / num_deliveries
+        
+        total_deliveries += num_deliveries
+
+        if route_distance < shortest_route_distance:
+            shortest_route_distance = route_distance
+            shortest_route = route_number
+        elif route_distance > longest_route_distance:
+            longest_route_distance = route_distance
+            longest_route = route_number
+        
+        print("** Statistics for route  #" + route_number + "**")
+        print("Shortest distance between deliveries: " + shortest_trip_distance)
+        print("Longest distance between deliveries: " + shortest_trip_distance)
+        print("Distance traveled: " + route_distance)
+        print("Average distance between deliveries: " + average_trip_distance)
+        
+    print("Total number of delivery routes: " + route_number)
+    print("Total number of deliveries: " + total_deliveries)
+    print("Total distance traveled: " + total_distance)
+    
+    print("Route # 1 has the shortest travel distance: " + shortest_route_distance)
+    print("Route # 1 has the longest travel distance: " + longest_route_distance)
+    
+    print("Normal Termination.")
 
 
 """
